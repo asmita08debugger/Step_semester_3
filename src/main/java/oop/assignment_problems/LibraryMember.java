64c1785a421b1dcb522a29f4de1d1ffb740bb074
lazy-debugger <as0464@srmist.edu.in>
2026-08-29 11:28:18 +0530

    Add F4 LibraryMember program

--- src/main/java/oop/assignment_problems/LibraryMember.java
+++ src/main/java/oop/assignment_problems/LibraryMember.java
@@ -1,5 +1,39 @@
 package main.java.oop.assignment_problems;
-
-public class LibraryMember {
-    
+class LibraryMember 
+{
+    String name;
+    int borrowedCount;
+    LibraryMember(String name, int borrowedCount) 
+    {
+        this.name = name;
+        this.borrowedCount = borrowedCount;
+    }
+    void borrowBook() 
+    {
+        if (borrowedCount < 5) 
+        {
+            borrowedCount++;
+            System.out.println(name + " borrowed a book.");
+        } 
+        else 
+        {
+            System.out.println("Borrowing limit reached.");
+        }
+    }
+    void returnBook() 
+    {
+        if (borrowedCount > 0) 
+        {
+            borrowedCount--;
+            System.out.println(name + " returned a book.");
+        }
+    }
+    public static void main(String[] args) 
+    {
+        LibraryMember m1 = new LibraryMember("Aditi", 4);
+        m1.borrowBook();
+        m1.borrowBook();
+        m1.returnBook();
+        System.out.println("Books currently borrowed: " + m1.borrowedCount);
+    }
 }
