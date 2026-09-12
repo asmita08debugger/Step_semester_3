c8e4d3c54dd866517d9153ef087583cc0f7b95b3
lazy-debugger <as0464@srmist.edu.in>
2026-09-12 10:04:50 +0530

    Add Week 1 F3 assignment problem

---
+++ src/main/java/string_concepts/assignment_problems/TrafficSignalStreakAnalyzer.java
@@ -0,0 +1,36 @@
+package main.java.string_concepts.assignment_problems;
+import java.util.Scanner;
+public class TrafficSignalStreakAnalyzer 
+{
+    static void findLongestStreak(String signalLog) 
+    {
+        char longestColor = signalLog.charAt(0);
+        int currentCount = 1;
+        int longestCount = 1;
+        for (int i = 1; i < signalLog.length(); i++) 
+        {
+            if (signalLog.charAt(i) == signalLog.charAt(i - 1)) 
+            {
+                currentCount++;
+            } 
+            else 
+            {
+                currentCount = 1;
+            }
+            if (currentCount > longestCount) 
+            {
+                longestCount = currentCount;
+                longestColor = signalLog.charAt(i);
+            }
+        }
+        System.out.println("Longest Streak: '" + longestColor + "' repeated " + longestCount + " times");
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter signal log: ");
+        String signalLog = sc.nextLine();
+        findLongestStreak(signalLog);
+        sc.close();
+    }
+}
